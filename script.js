dd70f43497e2ea492dfb76aa6980a05894864dbc
Rebeca Souza <rebeca_0213@hotmail.com>
2024-10-20 19:03:22 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const optionImage = document.querySelectorAll(".option-image");
+const container = document.querySelector(".container");
+const resultText = document.querySelector(".result-text");
+const computerResult = document.querySelector(".computer-result img");
+const userResult = document.querySelector(".user-result img");
+
+
+const computerSrcImg = ["images/rock.png", "images/paper.png", "images/scissor.png"];
+
+const winner = {
+    RR: "Empate",
+    RP: "Computador",
+    RS: "Você",
+    PP: "Empate",
+    PR: "Você",
+    PS: "Computador",
+    SS: "Empate",
+    SR: "Computador",
+    SP: "Você"
+}
+
+function handleOptionClick(event) {
+  const clickedImage = event.currentTarget;
+  const userIndex = Array.from(optionImage).indexOf(clickedImage)
+
+
+  container.classList.add("start");
+  resultText.textContent = "...";
+
+  userResult.src = computerResult.src = computerSrcImg[0]
+
+  setTimeout(() => {
+    container.classList.remove("start");
+
+    userResult.src = computerSrcImg[userIndex]
+
+    const randomNumber = Math.floor(Math.random() * computerSrcImg.length);
+    computerResult.src = computerSrcImg[randomNumber]
+
+    const userValue = ['R', 'P', 'S'][userIndex]
+    const computerValue = ['R', 'P', 'S'][randomNumber]
+    const userComputerResult = userValue + computerValue 
+    const finalResult = winner[userComputerResult]
+
+
+    resultText.textContent = userValue === computerValue ? "Empate" : finalResult + " Ganhou"
+  }, 2000);
+}
+
+optionImage.forEach((img) => {
+  img.addEventListener("click", handleOptionClick);
+});
